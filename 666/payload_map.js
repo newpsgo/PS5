d7ffe580a00e54d7f20119ae1554cf6d40aedcc4
newpsgo <yanchao1015@gmail.com>
2023-10-22 14:41:33 +0800

    666测试新方案离线

---
+++ 666/payload_map.js
@@ -0,0 +1,76 @@
+const payload_map =
+    [
+        {
+            displayTitle: 'HEN v1.2（仅支持4.03和4.50）',
+            description: '解锁运行游戏PS5-kstuff.bin',
+            fileName: 'ps5-kstuff.bin',
+            author: 'sleirsgoevy',
+            source: 'https://github.com/sleirsgoevy/ps4jb2/blob/ps5-403/ps5-kstuff.bin',
+            version: '1.1'
+        },
+		{
+            displayTitle: '清除浏览器缓存',
+            description: '如遇到缓存影响折腾成功率，清除后重新缓存',
+            fileName: 'cleancache.elf',
+            author: 'Storm21CH',
+            source: 'https://github.com/Storm21CH/PS5_Browser_appCache_remove',
+            version: '1.0'
+        },
+        // {
+        //     displayTitle: 'libhijacker game-patch (broken)',
+        //     description: 'Patches supported games to run at higher framerates, and adds debug menus to certain titles.',
+        //     fileName: 'daemon.elf',
+        //     author: 'illusion0001, astrelsky',
+        //     source: 'https://github.com/illusion0001/libhijacker/releases',
+        //     version: '1.121'
+        // },
+        // {
+            // displayTitle: 'PS5 Payload ELF 加载器',
+            // description: '用于加载插件，加载器会在后台运行',
+            // fileName: 'elfldr.elf',
+            // author: 'john-tornblom',
+            // source:'https://github.com/john-tornblom/ps5-payload-elfldr/releases',
+            // version: '0.5'
+        // },
+		{
+            displayTitle: 'FTP插件',
+            description: '端口号1337，后台持续运行',
+            fileName: 'ftps5-p.elf',
+            author: 'SiSTR0, zecoxao, EchoStretch',
+            source:'https://github.com/EchoStretch/FTPS5/releases',
+            version: '1.4'
+        },
+        // {
+            // displayTitle: 'FTP插件',
+            // description: '端口1337',
+            // fileName: 'ftps5-np.elf',
+            // author: 'SiSTR0, zecoxao, EchoStretch',
+            // source:'https://github.com/EchoStretch/FTPS5/releases',
+            // version: '1.4'
+        // },
+		{
+            displayTitle: '屏蔽系统更新',
+            description: '配合USB文件防止系统更新',
+            fileName: 'Disable-UPD.elf',
+            author: 'logic-68',
+            source: 'https://github.com/logic-68/Enable-Disable-UPD-PS5/releases',
+            version: '1.03'
+        },
+        // {
+            // displayTitle: 'Versions',
+            // description: 'Shows kernel build, os and sdk versions',
+            // fileName: 'versions.elf',
+            // author: '?',
+            // source:'https://mega.nz/folder/4xA2ATTI#6lHvye1JUfhxRGvqOva8fw/folder/gwhWmRrB',
+            // version: '1.0'
+        // },
+        // {
+            // displayTitle: 'GetOSVersion',
+            // description: 'very slow',
+            // fileName: 'getOsVersion.elf',
+            // author: 'todo',
+            // source:'https://mega.nz/folder/4xA2ATTI#6lHvye1JUfhxRGvqOva8fw/folder/gwhWmRrB',
+            // version: '1.0'
+        // },
+
+    ];

8216638a5ba02be9d220597eb74da21387c0839e
newpsgo <yanchao1015@gmail.com>
2023-10-29 05:18:00 +0800

    恢复老版本

--- onlinehost/one/payload_map.js
+++ onlinehost/one/payload_map.js
@@ -1,16 +1,32 @@
 const payload_map =
     [
         {
-            displayTitle: 'HEN v1.2（仅支持4.03 和 4.50）',
-            description: '解锁运行游戏PS5-kstuff.bin',
+            displayTitle: 'HEN（支持 4.03 和 4.50 ）',
+            description: '点击解锁运行游戏！！！PS5-kstuff.bin',
             fileName: 'ps5-kstuff-v1.1.bin',
             author: 'sleirsgoevy',
             source: 'https://github.com/sleirsgoevy/ps4jb2/blob/ps5-403/ps5-kstuff.bin',
             version: '1.1'
         },
+		// {
+            // displayTitle: 'etaHEN 1.0b（一键注入灵魂）',
+            // description: '多合一插件，一键解锁运行+屏蔽升级+FTP等，支持 4.03 和 4.50',
+            // fileName: 'etaHEN-1.0b.bin',
+            // author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
+            // source: 'https://github.com/LightningMods/etaHEN',
+            // version: '1.0'
+        // },
+		{
+            displayTitle: '伪装高版本（支持4.03）',
+            description: '先运行HEN！可伪装PS5/PS4最高版本，安装运行高版本PS4游戏',
+            fileName: 'test10_ok.elf',
+            author: 'Jaafar',
+            source:'https://twitter.com/jaf51744',
+            version: '1.0'
+        },
 		{
             displayTitle: '清除浏览器缓存',
-            description: 'PS5无法双清，遇到缓存问题使用本功能清除后重新缓存',
+            description: 'PS5无法双清，遇到连续报错等缓存问题使用本功能清除后重新缓存',
             fileName: 'Browser_appCache_remove.elf',
             author: 'Storm21CH',
             source:'https://github.com/Storm21CH/PS5_Browser_appCache_remove/blob/main/Browser_appCache_remove.elf',
@@ -18,7 +34,7 @@ const payload_map =
         },
 		{
             displayTitle: '解锁部分正版游戏120/60帧率',
-            description: 'v1.124版，运行后可直接启动相应游戏',
+            description: 'v1.124版，运行后可直接启动相应游戏（版本也需要对应）',
             fileName: 'libhijacker-spawner-1.124.elf;libhijacker-daemon-1.124.elf',
             author: 'illusion0001, astrelsky',
             source: 'https://github.com/illusion0001/libhijacker/releases',
@@ -33,14 +49,14 @@ const payload_map =
             // source:'https://github.com/john-tornblom/ps5-payload-elfldr/releases',
             // version: '0.5'
         // },
-		{
-            displayTitle: 'PS5 Payload ELF 加载器',
-            description: '注入端口号9021，用于加载ELF插件，加载器会在后台运行',
-            fileName: 'elfldr.elf',
-            author: 'john-tornblom',
-            source:'https://github.com/john-tornblom/ps5-payload-elfldr/releases',
-            version: '0.5'
-        },
+		// {
+            // displayTitle: 'Payload ELF 加载器',
+            // description: '注入端口号9021，用于加载ELF插件，加载器会在后台运行',
+            // fileName: 'elfldr.elf',
+            // author: 'john-tornblom',
+            // source:'https://github.com/john-tornblom/ps5-payload-elfldr/releases',
+            // version: '0.5'
+        // },
 		{
             displayTitle: 'FTP插件',
             description: '端口号1337，后台持续运行',
@@ -59,20 +75,20 @@ const payload_map =
         // },
 		{
             displayTitle: '屏蔽系统更新',
-            description: '使用方法咨询PSGO群',
+            description: '使用方法详见PSGO群',
             fileName: 'Disable-UPD.elf',
             author: 'logic-68',
             source: 'https://github.com/logic-68/Enable-Disable-UPD-PS5/releases',
             version: '1.03'
         },
-        // {
-            // displayTitle: 'Versions',
-            // description: 'Shows kernel build, os and sdk versions',
-            // fileName: 'versions.elf',
-            // author: '?',
-            // source:'https://mega.nz/folder/4xA2ATTI#6lHvye1JUfhxRGvqOva8fw/folder/gwhWmRrB',
-            // version: '1.0'
-        // },
+        {
+            displayTitle: '查看版本（确认是否伪装成功）',
+            description: '显示内核、系统、SDK等版本信息',
+            fileName: 'versions.elf',
+            author: '匿名作者',
+            source:'https://mega.nz/folder/4xA2ATTI#6lHvye1JUfhxRGvqOva8fw/folder/gwhWmRrB',
+            version: '1.0'
+        },
         // {
             // displayTitle: 'GetOSVersion',
             // description: 'very slow',

81115058c5624b4a30b7988ed9b1451bdfe63d07
newpsgo <yanchao1015@gmail.com>
2023-10-29 21:05:19 +0800

    一键折腾缓存推送1029

--- offlinehost/payload_map.js
+++ offlinehost/payload_map.js
@@ -1,16 +1,24 @@
 const payload_map =
     [
         {
-            displayTitle: 'HEN v1.2（仅支持4.03 和 4.50）',
-            description: '解锁运行游戏PS5-kstuff.bin',
+            displayTitle: 'HEN（支持 4.03 和 4.50 ）',
+            description: '点击解锁运行游戏！！！PS5-kstuff.bin',
             fileName: 'ps5-kstuff-v1.1.bin',
             author: 'sleirsgoevy',
             source: 'https://github.com/sleirsgoevy/ps4jb2/blob/ps5-403/ps5-kstuff.bin',
             version: '1.1'
         },
 		{
+            displayTitle: '屏蔽系统更新（推荐）',
+            description: '一键屏蔽无需USB文件，点击后左上角显示“blocker enable”则为生效',
+            fileName: 'lock_unlock_updates.elf',
+            author: 'logic-68',
+            source: 'https://github.com/logic-68/Enable-Disable-UPD-PS5/releases',
+            version: '1.0'
+        },
+		{
             displayTitle: '清除浏览器缓存',
-            description: 'PS5无法双清，遇到缓存问题使用本功能清除后重新缓存',
+            description: 'PS5无法双清，遇到连续报错等缓存问题使用本功能清除后重新缓存',
             fileName: 'Browser_appCache_remove.elf',
             author: 'Storm21CH',
             source:'https://github.com/Storm21CH/PS5_Browser_appCache_remove/blob/main/Browser_appCache_remove.elf',
@@ -18,12 +26,20 @@ const payload_map =
         },
 		{
             displayTitle: '解锁部分正版游戏120/60帧率',
-            description: 'v1.124版，运行后可直接启动相应游戏',
+            description: 'v1.124版，运行后可直接启动相应游戏（版本也需要对应）',
             fileName: 'libhijacker-spawner-1.124.elf;libhijacker-daemon-1.124.elf',
             author: 'illusion0001, astrelsky',
             source: 'https://github.com/illusion0001/libhijacker/releases',
             loader: 'libhijacker',
             version: '1.124'
+        },
+		{
+            displayTitle: 'etaHEN 金手指 1.0b',
+            description: '先运行本插件，后运行HEN。配合illusion的patches工具包进行使用',
+            fileName: 'etaHENwithcheats-1.0b.bin',
+            author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
+            source: 'https://github.com/LightningMods/etaHEN',
+            version: '1.0'
         },
         // {
             // displayTitle: 'PS5 Payload ELF 加载器',
@@ -33,14 +49,14 @@ const payload_map =
             // source:'https://github.com/john-tornblom/ps5-payload-elfldr/releases',
             // version: '0.5'
         // },
-		{
-            displayTitle: 'PS5 Payload ELF 加载器',
-            description: '注入端口号9021，用于加载ELF插件，加载器会在后台运行',
-            fileName: 'elfldr.elf',
-            author: 'john-tornblom',
-            source:'https://github.com/john-tornblom/ps5-payload-elfldr/releases',
-            version: '0.5'
-        },
+		// {
+            // displayTitle: 'Payload ELF 加载器',
+            // description: '注入端口号9021，用于加载ELF插件，加载器会在后台运行',
+            // fileName: 'elfldr.elf',
+            // author: 'john-tornblom',
+            // source:'https://github.com/john-tornblom/ps5-payload-elfldr/releases',
+            // version: '0.5'
+        // },
 		{
             displayTitle: 'FTP插件',
             description: '端口号1337，后台持续运行',
@@ -49,6 +65,14 @@ const payload_map =
             source:'https://github.com/EchoStretch/FTPS5/releases',
             version: '1.4'
         },
+		// {
+            // displayTitle: 'etaHEN 1.0b（多合一插件）',
+            // description: '一键解锁运行+屏蔽升级+FTP+伪装等，支持 4.03 和 4.50',
+            // fileName: 'etaHEN-1.0b.bin',
+            // author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
+            // source: 'https://github.com/LightningMods/etaHEN',
+            // version: '1.0'
+        // },
         // {
             // displayTitle: 'FTP插件',
             // description: '端口1337',
@@ -57,22 +81,22 @@ const payload_map =
             // source:'https://github.com/EchoStretch/FTPS5/releases',
             // version: '1.4'
         // },
-		{
-            displayTitle: '屏蔽系统更新',
-            description: '使用方法咨询PSGO群',
-            fileName: 'Disable-UPD.elf',
-            author: 'logic-68',
-            source: 'https://github.com/logic-68/Enable-Disable-UPD-PS5/releases',
-            version: '1.03'
-        },
-        // {
-            // displayTitle: 'Versions',
-            // description: 'Shows kernel build, os and sdk versions',
-            // fileName: 'versions.elf',
-            // author: '?',
-            // source:'https://mega.nz/folder/4xA2ATTI#6lHvye1JUfhxRGvqOva8fw/folder/gwhWmRrB',
+		// {
+            // displayTitle: '伪装PS5高版本（小白勿试）',
+            // description: '默认已经伪装PS4最高版本，如需伪装PS5最高版本需先点击运行HEN',
+            // fileName: 'test10_ok.elf',
+            // author: 'Jaafar',
+            // source:'https://twitter.com/jaf51744',
             // version: '1.0'
         // },
+        {
+            displayTitle: '查看版本',
+            description: '显示内核、系统、SDK等版本信息',
+            fileName: 'versions.elf',
+            author: '匿名作者',
+            source:'https://mega.nz/folder/4xA2ATTI#6lHvye1JUfhxRGvqOva8fw/folder/gwhWmRrB',
+            version: '1.0'
+        },
         // {
             // displayTitle: 'GetOSVersion',
             // description: 'very slow',

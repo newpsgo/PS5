05d4a6e86bc083504261310e019acc25d80444bb
newpsgo <yanchao1015@gmail.com>
2023-11-01 22:57:39 +0800

    全自动带伪装

--- offlinehostauto/payload_map.js
+++ offlinehostauto/payload_map.js
@@ -8,62 +8,20 @@ const payload_map =
             source: 'https://github.com/sleirsgoevy/ps4jb2/blob/ps5-403/ps5-kstuff.bin',
             version: '1.1'
         },
-        {
-            displayTitle: 'libhijacker game-patch',
-            description: '(v1.124) Patches supported games to run at higher framerates, and adds debug menus to certain titles.',
-            fileName: 'libhijacker-spawner-1.124.elf;libhijacker-daemon-1.124.elf',
-            author: 'illusion0001, astrelsky',
-            source: 'https://github.com/illusion0001/libhijacker/releases',
-            loader: 'libhijacker',
-            version: '1.124'
+		{
+            displayTitle: '伪装高版本（小白勿试）',
+            description: '用于安装运行高版本补丁，点击本插件前先点击运行HEN！',
+            fileName: 'TO99999999_403_450.bin',
+            author: 'Jaafar',
+            source:'https://twitter.com/jaf51744',
+            version: '1.0'
         },
-        {
-            displayTitle: 'PS5 Payload ELF Loader',
-            description: 'Uses port 9021. Persistent network elf loader',
-            fileName: 'elfldr.elf',
-            author: 'john-tornblom',
-            source:'https://github.com/john-tornblom/ps5-payload-elfldr/releases',
-            version: '0.5'
-        },
-        {
-            displayTitle: 'FTPS5 (Non-Persistent)',
-            description: 'FTP Server',
-            fileName: 'ftps5-np.elf',
-            author: 'SiSTR0, zecoxao, EchoStretch',
-            source:'https://github.com/EchoStretch/FTPS5/releases',
-            version: '1.4'
-        },
-        {
-            displayTitle: 'FTPS5 (Persistent)',
-            description: 'FTP Server',
-            fileName: 'ftps5-p.elf',
-            author: 'SiSTR0, zecoxao, EchoStretch',
-            source:'https://github.com/EchoStretch/FTPS5/releases',
-            version: '1.4'
-        },
-        {
-            displayTitle: 'Versions',
-            description: 'Shows kernel build, os and sdk versions',
+		{
+            displayTitle: '查看版本信息',
+            description: '显示内核、系统、SDK等版本信息',
             fileName: 'versions.elf',
-            author: '?',
+            author: '匿名作者',
             source:'https://mega.nz/folder/4xA2ATTI#6lHvye1JUfhxRGvqOva8fw/folder/gwhWmRrB',
             version: '1.0'
-        },
-        {
-            displayTitle: 'GetOSVersion',
-            description: 'very slow',
-            fileName: 'getOsVersion.elf',
-            author: 'todo',
-            source:'https://mega.nz/folder/4xA2ATTI#6lHvye1JUfhxRGvqOva8fw/folder/gwhWmRrB',
-            version: '1.0'
-        },
-        {
-            displayTitle: 'PS5_Browser_appCache_remove',
-            description: 'Removes appcache from browser.',
-            fileName: 'Browser_appCache_remove.elf',
-            author: 'Storm21CH',
-            source:'https://github.com/Storm21CH/PS5_Browser_appCache_remove/blob/main/Browser_appCache_remove.elf',
-            version: '1.0fix'
         }
-
     ];

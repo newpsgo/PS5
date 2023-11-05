44eb60b653cc71db07e02476b28e6aae2003cde9
newpsgo <yanchao1015@gmail.com>
2023-11-05 16:22:25 +0800

    全自动离线新增451

--- offlinehostauto/20231022/payload_map.js
+++ offlinehostauto/20231022/payload_map.js
@@ -3,7 +3,7 @@ const payload_map =
         {
             displayTitle: 'K-Stuff',
             description: 'FW 4.03 & 4.50 Only. FPKG enabler',
-            fileName: 'ps5-kstuff.bin',
+            fileName: 'ps5-kstuff-v1.1.bin',
             author: 'sleirsgoevy',
             source: 'https://github.com/sleirsgoevy/ps4jb2/blob/ps5-403/ps5-kstuff.bin',
             version: '1.1'

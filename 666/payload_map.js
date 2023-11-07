2fdc4eb87853a318954a6485c23b496e5d9f888a
newpsgo <yanchao1015@gmail.com>
2023-11-07 20:22:08 +0800

    一键1107v1

--- 666/payload_map.js
+++ 666/payload_map.js
@@ -1,12 +1,28 @@
 const payload_map =
     [
         {
+            displayTitle: 'etaHEN 普通版 1.1b（支持运行PS5）',
+            description: '一键解锁运行+屏蔽升级+FTP等，支持 4.03、4.50、4.51',
+            fileName: 'etaHEN-1.1b.bin',
+            author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
+            source: 'https://github.com/LightningMods/etaHEN',
+            version: '1.0'
+        },
+		{
             displayTitle: 'HEN 原版（推荐 / 支持 4.03、4.50、4.51）',
             description: '点击解锁运行游戏！！！PS5-kstuff.bin',
             fileName: 'ps5-kstuff.bin',
             author: 'sleirsgoevy',
             source: 'https://github.com/sleirsgoevy/ps4jb2/blob/ps5-403/ps5-kstuff.bin',
             version: '1.1'
+        },
+		{
+            displayTitle: 'etaHEN 金手指版 1.1b（与自制应用冲突）',
+            description: '普通版+金手指功能。配合illusion的patches工具包进行使用',
+            fileName: 'etaHENwithcheats-1.1b.bin',
+            author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
+            source: 'https://github.com/LightningMods/etaHEN',
+            version: '1.0'
         },
 		{
             displayTitle: '清除浏览器缓存',
@@ -16,22 +32,6 @@ const payload_map =
             source:'https://github.com/Storm21CH/PS5_Browser_appCache_remove/blob/main/Browser_appCache_remove.elf',
             version: '1.0fix'
         },
-		// {
-            // displayTitle: 'etaHEN 普通版 1.1b（多合一插件）',
-            // description: '一键解锁运行+屏蔽升级+FTP等，支持 4.03 和 4.50',
-            // fileName: 'etaHEN-1.1b.bin',
-            // author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
-            // source: 'https://github.com/LightningMods/etaHEN',
-            // version: '1.0'
-        // },
-		// {
-            // displayTitle: 'etaHEN 金手指版 1.1b（与自制应用冲突）',
-            // description: '普通版+金手指功能。配合illusion的patches工具包进行使用',
-            // fileName: 'etaHENwithcheats-1.1b.bin',
-            // author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
-            // source: 'https://github.com/LightningMods/etaHEN',
-            // version: '1.0'
-        // },
 		{
             displayTitle: '屏蔽系统更新（推荐）',
             description: '只需点击一次，左上角显示“blocker enable”则为永久生效',
@@ -42,17 +42,17 @@ const payload_map =
         }, 
         {
             displayTitle: '解锁部分正版游戏120/60帧率',
-            description: 'v1.124版，运行后可直接启动相应游戏（版本也需要对应）',
-            fileName: 'libhijacker-spawner-1.124.elf;libhijacker-daemon-1.124.elf',
+            description: 'v1.132版，运行后可直接启动相应游戏（版本也需要对应）',
+            fileName: 'spawner.elf',
             author: 'illusion0001, astrelsky',
             source: 'https://github.com/illusion0001/libhijacker/releases',
-            loader: 'libhijacker',
-            version: '1.124'
+            // loader: 'libhijacker',
+            version: '1.132'
         },
 		{
             displayTitle: '伪装高版本（小白勿试）',
             description: '用于安装运行高版本补丁，点击本插件前先点击运行HEN！',
-            fileName: 'TO99999999_403_450.bin',
+            fileName: 'TO99999999_403_450.elf',
             author: 'Jaafar',
             source:'https://twitter.com/jaf51744',
             version: '1.0'
@@ -69,7 +69,7 @@ const payload_map =
             displayTitle: '查看版本信息',
             description: '显示内核、系统、SDK等版本信息',
             fileName: 'versions.elf',
-            author: '匿名作者',
+            author: 'LM',
             source:'https://mega.nz/folder/4xA2ATTI#6lHvye1JUfhxRGvqOva8fw/folder/gwhWmRrB',
             version: '1.0'
         },

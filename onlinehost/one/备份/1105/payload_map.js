eacdd7617acd4e58cbe5dfda120a2b950a7e6bf2
newpsgo <yanchao1015@gmail.com>
2023-11-21 00:25:48 +0800

    一键在线1120

--- onlinehost/one/备份/1105/payload_map.js
+++ onlinehost/one/备份/1105/payload_map.js
@@ -1,40 +1,40 @@
 const payload_map =
     [
         {
-            displayTitle: 'HEN 原版（推荐 / 支持 4.03、4.50、4.51）',
+            displayTitle: 'HEN 原版（支持403、450、451）',
             description: '点击解锁运行游戏！！！PS5-kstuff.bin',
-            fileName: 'ps5-kstuff.bin',
+            fileName: 'ps5-kstuff-v1.1.bin',
             author: 'sleirsgoevy',
             source: 'https://github.com/sleirsgoevy/ps4jb2/blob/ps5-403/ps5-kstuff.bin',
-            version: '1.2'
-        },
-		{
-            displayTitle: 'etaHEN 第三方版 v1.1beta（即将支持运行PS5）',
-            description: '一键解锁运行游戏+HB商店等，支持 4.03、4.50、4.51',
-            fileName: 'etaHEN-1.1b.bin',
-            author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
-            source: 'https://github.com/LightningMods/etaHEN',
             version: '1.1'
         },
 		{
             displayTitle: '清除浏览器缓存',
-            description: 'PS5无法双清，遇到缓存异常问题使用本功能清除后重新缓存',
+            description: 'PS5无法双清，遇到连续报错等缓存问题使用本功能清除后重新缓存',
             fileName: 'Browser_appCache_remove.elf',
             author: 'Storm21CH',
             source:'https://github.com/Storm21CH/PS5_Browser_appCache_remove/blob/main/Browser_appCache_remove.elf',
             version: '1.0fix'
         },
-		{
-            displayTitle: 'etaHEN 金手指版 v1.1beta（与自制应用冲突）',
-            description: '普通版+金手指功能。配合illusion的patches工具包进行使用',
-            fileName: 'etaHENwithcheats-1.1b.bin',
-            author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
-            source: 'https://github.com/LightningMods/etaHEN',
-            version: '1.1'
-        },
+		// {
+            // displayTitle: 'etaHEN 普通版 1.1b（多合一插件）',
+            // description: '一键解锁运行+屏蔽升级+FTP等，支持 4.03 和 4.50',
+            // fileName: 'etaHEN-1.1b.bin',
+            // author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
+            // source: 'https://github.com/LightningMods/etaHEN',
+            // version: '1.0'
+        // },
+		// {
+            // displayTitle: 'etaHEN 金手指版 1.1b（与自制应用冲突）',
+            // description: '普通版+金手指功能。配合illusion的patches工具包进行使用',
+            // fileName: 'etaHENwithcheats-1.1b.bin',
+            // author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
+            // source: 'https://github.com/LightningMods/etaHEN',
+            // version: '1.0'
+        // },
 		{
             displayTitle: '屏蔽系统更新（推荐）',
-            description: '左上角显示“blocker enable”则为永久生效，建议配合路由器屏蔽',
+            description: '只需点击一次，左上角显示“blocker enable”则为永久生效',
             fileName: 'lock_unlock_updates.elf',
             author: 'logic-68',
             source: 'https://github.com/logic-68/Enable-Disable-UPD-PS5/releases',
@@ -42,24 +42,24 @@ const payload_map =
         }, 
         {
             displayTitle: '解锁部分正版游戏120/60帧率',
-            description: 'v1.136版，运行后可直接启动相应游戏（版本也需要对应）',
-            fileName: 'spawner.elf',
+            description: 'v1.124版，运行后可直接启动相应游戏（版本也需要对应）',
+            fileName: 'libhijacker-spawner-1.124.elf;libhijacker-daemon-1.124.elf',
             author: 'illusion0001, astrelsky',
             source: 'https://github.com/illusion0001/libhijacker/releases',
-            // loader: 'libhijacker',
-            version: '1.136'
+            loader: 'libhijacker',
+            version: '1.124'
         },
 		{
-            displayTitle: '伪装高版本（支持4.03、4.50 / 需点击两次）',
+            displayTitle: '伪装高版本（小白勿试）',
             description: '用于安装运行高版本补丁，点击本插件前先点击运行HEN！',
-            fileName: 'TO99999999_403_450.elf',
+            fileName: 'TO99999999_403_450.bin',
             author: 'Jaafar',
             source:'https://twitter.com/jaf51744',
-            version: '1.1'
+            version: '1.0'
         },
 		{
-            displayTitle: 'FTP远程文件管理',
-            description: '端口号1337，后台持续运行，配合FileZilla',
+            displayTitle: 'FTP插件',
+            description: '端口号1337，后台持续运行',
             fileName: 'ftps5-p.elf',
             author: 'SiSTR0, zecoxao, EchoStretch',
             source:'https://github.com/EchoStretch/FTPS5/releases',
@@ -69,7 +69,7 @@ const payload_map =
             displayTitle: '查看版本信息',
             description: '显示内核、系统、SDK等版本信息',
             fileName: 'versions.elf',
-            author: 'LM',
+            author: '匿名作者',
             source:'https://mega.nz/folder/4xA2ATTI#6lHvye1JUfhxRGvqOva8fw/folder/gwhWmRrB',
             version: '1.0'
         },

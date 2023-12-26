95c7a3d81e5cea28598d5ad74942c31879a4d815
newpsgo <yanchao1015@gmail.com>
2023-12-26 08:57:22 +0800

    在线版1226

--- onlinehost/one/payload_map.js
+++ onlinehost/one/payload_map.js
@@ -1,20 +1,44 @@
 const payload_map =
     [
         {
-            displayTitle: 'HEN 原版（推荐 / 支持 4.03、4.50、4.51）',
-            description: '点击解锁运行游戏！！！PS5-kstuff.bin',
+            displayTitle: 'HEN 原版（推荐 / 仅解锁运行PS4）',
+            description: '点击或按下手柄L2键！解锁运行游戏！！！支持 4.03、4.5X',
             fileName: 'ps5-kstuff.bin',
             author: 'sleirsgoevy',
             source: 'https://github.com/sleirsgoevy/ps4jb2/blob/ps5-403/ps5-kstuff.bin',
             version: '1.2'
         },
 		{
-            displayTitle: 'etaHEN 第三方版 v1.1beta（即将支持运行PS5）',
-            description: '一键解锁运行游戏+HB商店等，支持 4.03、4.50、4.51',
-            fileName: 'etaHEN-1.1b.bin',
+            displayTitle: 'etaHEN 多合一版 v1.2b（解锁运行PS5/PS4）',
+            description: '点击或按下手柄R2键！支持itemzFlow、FTP、HB、金手指等，支持 4.03、4.5X',
+            fileName: 'etaHEN.bin',
             author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
             source: 'https://github.com/LightningMods/etaHEN',
-            version: '1.1'
+            version: '1.2'
+        },
+		{
+            displayTitle: 'PS5debug（支持金手指）',
+            description: '配合MultiTrainer II电脑客户端或内存调试工具',
+            fileName: 'ps5debug.elf',
+            author: 'SiSTR0, ctn123',
+            source: 'https://github.com/GoldHEN/ps5debug',
+            version: '1.0b1'
+        },
+		{
+            displayTitle: 'FTP远程文件管理',
+            description: '端口号1337，后台持续运行，配合FileZilla电脑客户端',
+            fileName: 'ftps5-p.elf',
+            author: 'SiSTR0, zecoxao, EchoStretch',
+            source:'https://github.com/EchoStretch/FTPS5/releases',
+            version: '1.4'
+        },
+		{
+            displayTitle: 'elf插件加载器（会导致断电）',
+            description: '端口号9021，后台持续运行，配合NetCat或安卓客户端',
+            fileName: 'elfldr.elf',
+            author: 'john-tornblom',
+            source:'https://github.com/john-tornblom/ps5-payload-elfldr/releases',
+            version: '0.5'
         },
 		{
             displayTitle: '清除浏览器缓存',
@@ -24,14 +48,14 @@ const payload_map =
             source:'https://github.com/Storm21CH/PS5_Browser_appCache_remove/blob/main/Browser_appCache_remove.elf',
             version: '1.0fix'
         },
-		{
-            displayTitle: 'etaHEN 金手指版 v1.1beta（与自制应用冲突）',
-            description: '普通版+金手指功能。配合illusion的patches工具包进行使用',
-            fileName: 'etaHENwithcheats-1.1b.bin',
-            author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
-            source: 'https://github.com/LightningMods/etaHEN',
-            version: '1.1'
-        },
+		// {
+            // displayTitle: 'etaHEN 金手指版 v1.1beta（与自制应用冲突）',
+            // description: '普通版+金手指功能。配合illusion的patches工具包进行使用',
+            // fileName: 'etaHENwithcheats-1.1b.bin',
+            // author: 'LightningMods_, sleirsgoevy, ChendoChap, astrelsky, illusion',
+            // source: 'https://github.com/LightningMods/etaHEN',
+            // version: '1.1'
+        // },
 		{
             displayTitle: '屏蔽系统更新（推荐）',
             description: '左上角显示“blocker enable”则为永久生效，建议配合路由器屏蔽',
@@ -41,13 +65,12 @@ const payload_map =
             version: '1.0'
         }, 
         {
-            displayTitle: '解锁部分正版游戏120/60帧率',
-            description: 'v1.136版，运行后可直接启动相应游戏（版本也需要对应）',
-            fileName: 'spawner.elf',
-            author: 'illusion0001, astrelsky',
-            source: 'https://github.com/illusion0001/libhijacker/releases',
-            // loader: 'libhijacker',
-            version: '1.136'
+            displayTitle: '备份数据（解决游戏图标消失问题）',
+            description: '索引、存档等数据，点击前插入exfat格式U盘，建议每月备份一次',
+            fileName: 'Backup-db-PS5.elf',
+            author: 'logic-68',
+            source:'https://github.com/logic-68/Backup-DB-PS5',
+            version: '0.5'
         },
 		{
             displayTitle: '伪装高版本（支持4.03、4.50 / 需点击两次）',
@@ -58,16 +81,17 @@ const payload_map =
             version: '1.1'
         },
 		{
-            displayTitle: 'FTP远程文件管理',
-            description: '端口号1337，后台持续运行，配合FileZilla',
-            fileName: 'ftps5-p.elf',
-            author: 'SiSTR0, zecoxao, EchoStretch',
-            source:'https://github.com/EchoStretch/FTPS5/releases',
-            version: '1.4'
+            displayTitle: '解锁部分正版游戏120/60帧率',
+            description: 'v1.138 运行后可直接启动相应游戏（版本也需要对应）',
+            fileName: 'spawner.elf',
+            author: 'illusion0001, astrelsky',
+            source: 'https://github.com/illusion0001/libhijacker/releases',
+            // loader: 'libhijacker',
+            version: '1.136'
         },
 		{
             displayTitle: '查看版本信息',
-            description: '显示内核、系统、SDK等版本信息',
+            description: '内核、系统、SDK等版本信息',
             fileName: 'versions.elf',
             author: 'LM',
             source:'https://mega.nz/folder/4xA2ATTI#6lHvye1JUfhxRGvqOva8fw/folder/gwhWmRrB',
@@ -75,7 +99,7 @@ const payload_map =
         },
 		{
             displayTitle: '查看系统状态',
-            description: '温度、频率等',
+            description: '温度、频率等系统信息',
             fileName: 'hwinfo-tornblom.elf',
             author: '匿名作者',
             source:'?',

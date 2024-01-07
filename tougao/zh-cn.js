f32f7eaaca0d3229a431afcdde95c327cadef356
newpsgo <yanchao1015@gmail.com>
2024-01-07 14:32:14 +0800

    更新 zh-cn.js

--- tougao/zh-cn.js
+++ tougao/zh-cn.js
@@ -1,20 +1,20 @@
 window.CUSDIS_LOCALE = {
   powered_by: ' ',
-  post_comment: '发送',
-  loading: '正在加载全部投稿内容',
+  post_comment: '提交',
+  loading: '正在加载全部投稿内容...',
 
   // comment
   email: '邮箱地址 (可不填)',
   nickname: '昵称',
-  reply_placeholder: '投稿内容',
+  reply_placeholder: '内容',
   reply_btn: '回复',
   sending: '发送中…',
 
   // reply
   mod_badge: '管理员',
-  content_is_required: '投稿内容不能为空',
+  content_is_required: '内容不能为空',
   nickname_is_required: '昵称不能为空',
 
   comment_has_been_sent:
-    '投稿已发送，管理员审核通过后会展示',
+    '内容已发送，管理员审核通过后会展示',
 }

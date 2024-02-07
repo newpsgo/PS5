17bf91cd10a17136dba7d6d27b323a2cc862d493
newpsgo <yanchao1015@gmail.com>
2024-02-08 07:47:30 +0800

    更新 denglong.js

--- css/denglong.js
+++ css/denglong.js
@@ -21,7 +21,7 @@ function l() {
   const e = document.createElement("div");
   e.className = "lantern-left";
   const t = document.createElement("div");
-  t.className = "lantern-right", e.innerHTML = a("龍運"), t.innerHTML = a("當頭");
+  t.className = "lantern-right", e.innerHTML = a("福禄"), t.innerHTML = a("两旺");
   const n = document.createElement("div");
   n.className = "lantern", n.appendChild(e), n.appendChild(t), document.body.appendChild(n);
 }

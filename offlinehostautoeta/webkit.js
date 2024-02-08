a0c31ea85386decd980006fb81e434f58a135f44
newpsgo <yanchao1015@gmail.com>
2024-02-09 01:16:51 +0800

    恢复0206

--- offlinehostautoeta/webkit.js
+++ offlinehostautoeta/webkit.js
@@ -15,8 +15,8 @@ function hex(n) {
     return "0x" + (new Number(n)).toString(16);
 }
 
-async function run_fontface() {
-    print("[+] Webkit exploit (FontFace)");
+async function poc() {
+    print("[+] Running webkit exploit...");
     await sleep(0); // trigger rerender
 
     var union = new ArrayBuffer(8);

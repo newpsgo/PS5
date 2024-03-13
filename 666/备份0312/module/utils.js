827f11d332b8ed00fce3b4ff023449b32da81f3d
newpsgo <yanchao1015@gmail.com>
2024-03-13 20:38:20 +0800

    psfree去弹窗测试

--- 666/备份0312/module/utils.js
+++ 666/备份0312/module/utils.js
@@ -21,9 +21,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.  */
 function die(msg) {
     // alert(msg);
     // undefinedFunction();
-    // throw new Error("PSFree failed: " + msg);
-	print('Next loading...');
-	document.location.href = "cache.html";
+    throw new Error("PSFree执行失败: " + msg + "\n 请长按PS键回到桌面并重新进入本页面");
 }
 
 function debug_log(msg) {

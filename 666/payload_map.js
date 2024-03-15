ce82ce785316b393cd1dd59b810a828a30305e82
newpsgo <yanchao1015@gmail.com>
2024-03-15 19:39:39 +0800

    psree+eta+del

--- 666/payload_map.js
+++ 666/payload_map.js
@@ -1,27 +1,11 @@
 const payload_map =
     [
         {
-            displayTitle: 'K-Stuff',
-            description: 'FW 4.03 & 4.50 Only. FPKG enabler',
-            fileName: 'ps5-kstuff.bin',
-            author: 'sleirsgoevy',
-            source: 'https://github.com/sleirsgoevy/ps4jb2/blob/ps5-403/ps5-kstuff.bin',
-            version: '1.2'
+            displayTitle: 'etaHEN',
+            description: 'AIO payload',
+            fileName: 'etaHEN.bin',
+            author: 'LM, illusion0001, astrelsky, sleirsgoevy',
+            source: 'https://github.com/LightningMods/etaHEN',
+            version: '1.x'
         }
-		// {
-            // displayTitle: 'ps5debug',
-            // description: 'FW 4.03 & 4.50 Only. FPKG enabler',
-            // fileName: 'ps5debug.elf',
-            // author: 'sistr0',
-            // source: 'https://github.com/GoldHEN/ps5debug/releases',
-            // version: '1.0b'
-        // },
-		// {
-            // displayTitle: 'FTPS5 (Persistent)',
-            // description: 'FTP Server',
-            // fileName: 'ftps5-p.elf',
-            // author: 'SiSTR0, zecoxao, EchoStretch',
-            // source:'https://github.com/EchoStretch/FTPS5/releases',
-            // version: '1.4'
-        // }
     ];
